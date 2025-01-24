c28d38cd4bf457f7ce11c6c11b4cd6367c84e581
fasih-nasir <fasihnasir12@gmail.com>
2025-01-24 14:52:55 +0500

    c

--- src/app/todos/page.js
+++ src/app/todos/page.js
@@ -1,28 +1,36 @@
-// "use server"
+// // "use server"
 
-import { addtodo } from "../actions/todo";
-import List from "../component/list";
+// import { addtodo } from "../actions/todo";
+// import List from "../component/list";
 
-export default async function Todos(){
+// export default async function Todos(){
 
-    let res= await fetch("http://localhost:3000/api/todos",{cache:"no-cache"});
-  res = await res.json();
+//     let res= await fetch("http://localhost:3000/api/todos",{cache:"no-cache"});
+//   res = await res.json();
 
 
-  return(
-        <>
-        <div>
-            <h2>todo</h2>
-            {res.data?.map((e)=>(         
-                  <List todo={e} key={e.id}/>
+//   return(
+//         <>
+//         <div>
+//             <h2>todo</h2>
+//             {res.data?.map((e)=>(         
+//                   <List todo={e} key={e.id}/>
             
-           ))}
-        </div>
+//            ))}
+//         </div>
 
-        <form action={addtodo}>
-            <input type="text" placeholder="enter " name="name" ></input>
-<input className="" type="submit"  value={"add todo"} />
-        </form>
-        </>
-    )
-} 
+//         <form action={addtodo}>
+//             <input type="text" placeholder="enter " name="name" ></input>
+// <input className="" type="submit"  value={"add todo"} />
+//         </form>
+//         </>
+//     )
+// } 
+
+export default function Todos() {
+  return (
+      <div>
+          <h1>Todos Page</h1>
+      </div>
+  );
+}
